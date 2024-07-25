68adcac91ab14207bc6f5376d8a94dbc26e3c776
Evgeniy Simonenko <tiikara93@gmail.com>
2024-07-25 16:31:22 +1000

    Migrate Unizero config for Tikara Atari
    Deleted the outdated `tikara_atari_unizero_stack4_config.py` and created `tikara_atari_unizero_config.py` with an updated configuration structure. Improved the organization and readability of the configuration settings to align with the current requirements.

--- zoo/atari/config/tikara/tikara_atari_unizero_config.py
+++ zoo/atari/config/tikara/tikara_atari_unizero_config.py
@@ -1,6 +1,5 @@
 from easydict import EasyDict
 from zoo.atari.config.atari_env_action_space_map import atari_env_action_space_map
-from zoo.atari.config.atari_unizero_config import atari_unizero_config, atari_unizero_create_config
 
 env_id = 'PongNoFrameskip-v4'  # You can specify any Atari game here
 action_space_size = atari_env_action_space_map[env_id]
@@ -8,12 +7,14 @@ action_space_size = atari_env_action_space_map[env_id]
 # ==============================================================
 # begin of the most frequently changed config specified by the user
 # ==============================================================
+update_per_collect = None
+replay_ratio = 0.25
 collector_env_num = 8
 n_episode = 8
 evaluator_env_num = 3
-update_per_collect = None
-replay_ratio = 0.25
-reanalyze_ratio = 0
+num_simulations = 50
+max_env_step = int(1e6)
+reanalyze_ratio = 0.
 batch_size = 64
 # ==============================================================
 # Размер контекста при обучении. Определяет размер максимальный размер контекста сети
@@ -23,44 +24,50 @@ num_unroll_steps = 10
 # Определяет, сколько модель использует прошлых шагов.
 # ==============================================================
 infer_context_length = 4
-max_env_step = int(5e5)
-num_simulations = 50
+
+# ====== only for debug =====
+# collector_env_num = 2
+# n_episode = 2
+# evaluator_env_num = 2
+# num_simulations = 5
+# max_env_step = int(5e5)
+# reanalyze_ratio = 0.
+# batch_size = 2
+# num_unroll_steps = 10
 # ==============================================================
 # end of the most frequently changed config specified by the user
 # ==============================================================
 
-atari_unizero_stack4_config = EasyDict(atari_unizero_config)  # Copy the base config
-
-# Modify specific parts for stack4
-atari_unizero_stack4_config.exp_name = f'data_unizero/{env_id[:-14]}_stack4_unizero_upc{update_per_collect}-rr{replay_ratio}_H{num_unroll_steps}_bs{batch_size}_seed0'
-atari_unizero_stack4_config.env.update(
-    dict(
-        observation_shape=(4, 64, 64),
-        gray_scale=True,
-        collect_max_episode_steps=int(2e4),
-        eval_max_episode_steps=int(1e4),
+atari_unizero_config = dict(
+    learn=dict(
+        learner=dict(
+            hook=dict(
+                save_ckpt_after_iter=4000,
+            )
+        ),
+    ),
+    env=dict(
+        stop_value=int(1e6),
+        env_id=env_id,
+        observation_shape=(3, 64, 64),
+        gray_scale=False,
+        collector_env_num=collector_env_num,
+        evaluator_env_num=evaluator_env_num,
+        n_evaluator_episode=evaluator_env_num,
+        manager=dict(shared_memory=False, ),
         save_replay=True,
         replay_path='/mnt/d/source/LightZero/data_unizero/replay',
-    )
-)
-atari_unizero_stack4_config.policy.update(
-    dict(
-        learn=dict(
-            learner=dict(
-                hook=dict(
-                    save_ckpt_after_iter=4000,
-                )
-            ),
-        ),
+        # TODO: only for debug
+        # collect_max_episode_steps=int(50),
+        # eval_max_episode_steps=int(50),
+    ),
+    policy=dict(
         model=dict(
-            observation_shape=(4, 64, 64),
-            image_channel=1,
-            frame_stack_num=4,
-            gray_scale=True,
+            observation_shape=(3, 64, 64),
             action_space_size=action_space_size,
             world_model_cfg=dict(
                 max_blocks=num_unroll_steps,
-                max_tokens=2 * num_unroll_steps,
+                max_tokens=2 * num_unroll_steps,  # NOTE: each timestep has 2 tokens: obs and action
                 context_length=2 * infer_context_length,
                 device='cuda',
                 # device='cpu',
@@ -73,7 +80,8 @@ atari_unizero_stack4_config.policy.update(
             ),
         ),
         # (str) The path of the pretrained model. If None, the model will be initialized by the default model.
-        model_path='/mnt/d/source/LightZero/data_unizero/001_Pong_stack4_unizero_upcNone-rr0.25_H10_bs64_seed0/ckpt/iteration_126123.pth.tar',
+        model_path=None,
+        num_unroll_steps=num_unroll_steps,
         update_per_collect=update_per_collect,
         replay_ratio=replay_ratio,
         batch_size=batch_size,
@@ -84,15 +92,30 @@ atari_unizero_stack4_config.policy.update(
         replay_buffer_size=int(1e6),
         collector_env_num=collector_env_num,
         evaluator_env_num=evaluator_env_num,
-    )
+    ),
 )
-main_config = atari_unizero_stack4_config
+atari_unizero_config = EasyDict(atari_unizero_config)
+main_config = atari_unizero_config
+
+atari_unizero_create_config = dict(
+    env=dict(
+        type='atari_lightzero',
+        import_names=['zoo.atari.envs.atari_lightzero_env'],
+    ),
+    env_manager=dict(type='subprocess'),
+    policy=dict(
+        type='unizero',
+        import_names=['lzero.policy.unizero'],
+    ),
+)
+atari_unizero_create_config = EasyDict(atari_unizero_create_config)
 create_config = atari_unizero_create_config
 
 if __name__ == "__main__":
+    # Define a list of seeds for multiple runs
     seeds = [0]  # You can add more seed values here
     for seed in seeds:
         # Update exp_name to include the current seed
-        main_config.exp_name = f'data_unizero/{env_id[:-14]}_stack4_unizero_upc{update_per_collect}-rr{replay_ratio}_H{num_unroll_steps}_bs{batch_size}_seed{seed}'
+        main_config.exp_name = f'data_unizero/{env_id[:-14]}_stack1_unizero_upc{update_per_collect}-rr{replay_ratio}_H{num_unroll_steps}_bs{batch_size}_seed{seed}'
         from lzero.entry import train_unizero
-        train_unizero([main_config, create_config], model_path=main_config.policy.model_path, seed=seed, max_env_step=max_env_step)
+        train_unizero([main_config, create_config], seed=seed, model_path=main_config.policy.model_path, max_env_step=max_env_step)
