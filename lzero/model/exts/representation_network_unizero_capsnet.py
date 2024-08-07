Fix integer division in out_capsules tensor shape calculation
Changed the division operator from a single slash to a double slash to ensure integer division in the `out_capsules` calculation. This prevents potential issues with tensor shapes when `embedding_dim` is not evenly divisible by 32.

lzero/model/exts/representation_network_unizero_capsnet.py
@@ -94,7 +94,7 @@ class RepresentationNetworkUniZeroCapsnet(nn.Module):
         self.activation = activation
         self.embedding_dim = embedding_dim
 
-        self.out_capsules = (32, self.embedding_dim / 32) # 32 x (embedding_dim / 32) = embedding_dim
+        self.out_capsules = (32, self.embedding_dim // 32) # 32 x (embedding_dim / 32) = embedding_dim
         self.head = nn.Sequential(
             CapsInitialModule(
                 in_channels=num_channels,