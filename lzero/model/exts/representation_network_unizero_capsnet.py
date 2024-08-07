34f27928414dc737308befe24d716ca259972459
Evgeniy Simonenko <tiikara93@gmail.com>
2024-08-07 11:51:49 +1000

    Add RepresentationNetworkUniZeroCapsnet model
    Introduce a new neural network model, RepresentationNetworkUniZeroCapsnet, to handle 2D image observations and encode them into a latent state. This model includes features like downsampling, residual blocks, simplicial normalization, and capsule networks for enhanced performance.

---
+++ lzero/model/exts/representation_network_unizero_capsnet.py
@@ -0,0 +1,155 @@
+import math
+from dataclasses import dataclass
+from typing import Optional, Tuple
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+from ding.torch_utils import MLP, ResBlock
+from ding.utils import SequenceType
+from ditk import logging
+from lzero.model.common import SimNorm
+from lzero.model.common import NormByType
+from functools import partial
+from .efficientnet_v2 import MBConvConfig, MBConv, ConvBNAct
+from .capsnet_layers import PrimaryCaps, RoutingCaps
+import math
+from functools import partial
+from collections import OrderedDict
+
+from lzero.model.common import DownSample
+
+import torch
+from torch import nn
+import timm
+
+class RepresentationNetworkUniZeroCapsnet(nn.Module):
+
+    def __init__(
+            self,
+            observation_shape: SequenceType = (3, 64, 64),
+            num_res_blocks: int = 1,
+            activation: nn.Module = nn.GELU(approximate='tanh'),
+            norm_type: str = 'BN',
+            embedding_dim: int = 256,
+            group_size: int = 8,
+            downsample: bool = True,
+            num_channels: int = 64,
+    ) -> None:
+        """
+        Overview:
+            Representation network used in UniZero. Encode the 2D image obs into latent state.
+            Currently, the network only supports obs images with both a width and height of 64.
+        Arguments:
+            - observation_shape (:obj:`SequenceType`): The shape of observation space, e.g. [C, W, H]=[3, 64, 64]
+                for video games like atari, RGB 3 channel.
+            - num_res_blocks (:obj:`int`): The number of residual blocks.
+            - num_channels (:obj:`int`): The channel of output hidden state.
+            - downsample (:obj:`bool`): Whether to do downsampling for observations in ``representation_network``, \
+                defaults to True. This option is often used in video games like Atari. In board games like go, \
+                we don't need this module.
+            - activation (:obj:`nn.Module`): The activation function used in network, defaults to nn.ReLU(inplace=True). \
+                Use the inplace operation to speed up.
+            - norm_type (:obj:`str`): The type of normalization in networks. defaults to 'BN'.
+            - embedding_dim (:obj:`int`): The dimension of the latent state.
+            - group_size (:obj:`int`): The dimension for simplicial normalization.
+        """
+        super().__init__()
+        assert norm_type in ['BN', 'LN'], "norm_type must in ['BN', 'LN']"
+        logging.info(f"Using norm type: {norm_type}")
+        logging.info(f"Using activation type: {activation}")
+
+        self.observation_shape = observation_shape
+        self.downsample = downsample
+        if self.downsample:
+            self.downsample_net = DownSample(
+                observation_shape,
+                num_channels,
+                activation=activation,
+                norm_type=norm_type,
+            )
+        else:
+            self.conv = nn.Conv2d(observation_shape[0], num_channels, kernel_size=3, stride=1, padding=1, bias=False)
+
+            if norm_type == 'BN':
+                self.norm = nn.BatchNorm2d(num_channels)
+            elif norm_type == 'LN':
+                if downsample:
+                    self.norm = nn.LayerNorm(
+                        [num_channels, math.ceil(observation_shape[-2] / 16), math.ceil(observation_shape[-1] / 16)],
+                        eps=1e-5)
+                else:
+                    self.norm = nn.LayerNorm([num_channels, observation_shape[-2], observation_shape[-1]], eps=1e-5)
+
+        self.resblocks = nn.ModuleList(
+            [
+                ResBlock(
+                    in_channels=num_channels, activation=activation, norm_type=norm_type, res_type='basic', bias=False
+                ) for _ in range(num_res_blocks)
+            ]
+        )
+        self.activation = activation
+        self.embedding_dim = embedding_dim
+
+        out_channels = 64
+        caps_channels = 256
+        self.out_capsules = (32, 16)
+
+        self.expand = nn.Sequential(
+            nn.Conv2d(
+                in_channels=out_channels,
+                out_channels=caps_channels-out_channels,
+                kernel_size=1,
+                bias=False
+            ),
+            nn.BatchNorm2d(caps_channels-out_channels),
+            self.activation
+        )
+
+        self.caps = nn.Sequential(
+            PrimaryCaps(
+                in_channels=caps_channels,
+                kernel_size=observation_shape[1] // 8,
+                capsule_size=(32, 8),
+                bias=False
+            ),
+            RoutingCaps(
+                in_capsules=(32, 8),
+                out_capsules=self.out_capsules,
+                bias=False
+            )
+        )
+
+        self.head = nn.Sequential(
+            nn.Linear(
+                self.out_capsules[0] * self.out_capsules[1],
+                self.embedding_dim,
+                bias=False
+            ),
+            nn.BatchNorm1d(self.embedding_dim)
+        )
+
+        self.out_create_layers = []
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Shapes:
+            - x (:obj:`torch.Tensor`): :math:`(B, C_in, W, H)`, where B is batch size, C_in is channel, W is width, \
+                H is height.
+            - output (:obj:`torch.Tensor`): :math:`(B, C_out, W_, H_)`, where B is batch size, C_out is channel, W_ is \
+                output width, H_ is output height.
+        """
+        x = self.downsample_net(x)
+        for block in self.resblocks:
+            x = block(x)
+
+        x = torch.cat([x, self.expand(x)], dim=1)
+
+        x = self.caps(x)
+
+        x = self.head(x.reshape(-1, self.out_capsules[0] * self.out_capsules[1]))
+
+        return x
