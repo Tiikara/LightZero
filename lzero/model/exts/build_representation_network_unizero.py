0048e699c0f9cfe8d8032ed0bc5c7ab709b116bb
Evgeniy Simonenko <tiikara93@gmail.com>
2024-08-11 12:15:48 +1000

    refactor

--- lzero/model/exts/build_representation_network_unizero.py
+++ lzero/model/exts/build_representation_network_unizero.py
@@ -1,4 +1,5 @@
 from .representation_network_unizero_capsnet_coord import RepresentationNetworkUniZeroCapsnetCoord
+from .representation_network_unizero_capsnet import RepresentationNetworkUniZeroCapsnet
 
 def build_representation_network_unizero(
         observation_shape,
@@ -11,6 +12,16 @@ def build_representation_network_unizero(
 ):
     if model_config.type == 'capsnet_coord':
         return RepresentationNetworkUniZeroCapsnetCoord(
+            observation_shape = observation_shape,
+            num_res_blocks = num_res_blocks,
+            activation = activation,
+            norm_type = norm_type,
+            embedding_dim = embedding_dim,
+            group_size = group_size,
+            num_capsules = model_config.num_capsules
+        )
+    elif model_config.type == 'capsnet':
+        return RepresentationNetworkUniZeroCapsnet(
             observation_shape = observation_shape,
             num_res_blocks = num_res_blocks,
             activation = activation,
